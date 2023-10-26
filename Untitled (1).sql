07addc1dacdbd7d772779291b5b26b31b1a99491
FabinhoStriker32 <146888542+FabinhoStriker32@users.noreply.github.com>
2023-10-26 20:12:20 -0300

    MySql_Atualizado
    Todos exercicios.

--- Untitled (1).sql
+++ Untitled (1).sql
@@ -239,3 +239,58 @@ UPDATE medico SET em_atividade = 'Inativo' WHERE id_medico = 1;
 
 UPDATE medico SET em_atividade = 'Em atividade' WHERE id_medico = 10;
 ALTER TABLE medico ALTER COLUMN em_atividade SET DEFAULT 'Em atividade';
+
+ALTER TABLE medico ADD COLUMN em_atividade VARCHAR(20);
+UPDATE medico SET em_atividade = 'Inativo' WHERE id_medico = 1;
+
+UPDATE medico SET em_atividade = 'Em atividade' WHERE id_medico = 10;
+ALTER TABLE medico ALTER COLUMN em_atividade SET DEFAULT 'Em atividade';
+
+SELECT * FROM consulta WHERE YEAR(data_consulta) = 2020 OR id_convenio = 1;
+SELECT AVG(valor) FROM consulta WHERE YEAR(data) = 2020 OR convenio = 1;
+
+SELECT * FROM internacao WHERE data_alta > data_prev_alta;
+
+SELECT * FROM receita WHERE consulta_id = (SELECT MIN(id_receita) FROM consulta);
+
+SELECT * FROM consulta WHERE id_convenio = NULL ORDER BY valor DESC;
+SELECT * FROM consulta WHERE id_convenio = NULL ORDER BY valor ASC;
+
+SELECT i.id_internacao, i.data_entrada, i.data_prev_alta, i.data_alta, i.procedimento,
+       q.numero AS numero_quarto, t.valor_diaria,
+       DATEDIFF(i.data_alta, i.data_entrada) AS num_dias,
+       DATEDIFF(i.data_alta, i.data_entrada) * t.valor_diaria AS total_internacao
+FROM internacao i
+JOIN quarto q ON i.id_internacao = q.id_quarto
+JOIN tipo_quarto t ON q.id_tipoquarto = t.id_tipoquarto;
+
+SELECT p.nome AS nome_paciente, c.data_consulta, e.nome AS especialidade
+FROM consulta c
+JOIN paciente p ON c.id_paciente = p.id_paciente
+JOIN especialidade e ON c.especialidade_id = e.id_especialidade
+WHERE TIMESTAMPDIFF(YEAR, STR_TO_DATE(p.data_nascimento, '%Y-%m-%d'), c.data_consulta) < 18
+AND e.nome != 'Pediatria'
+ORDER BY c.data_consulta;
+
+SELECT p.nome AS NomePaciente, m.nome AS NomeMedico, i.data_internacao, i.procedimentos
+FROM internacao i
+JOIN medico m ON i.id_medico = m.id_medico
+JOIN paciente p ON i.id_paciente = p.id_paciente
+JOIN especialidade_medico em ON m.id_medico = em.id_medico
+JOIN especialidade e ON em.id_especialidade = e.id_especialidade
+WHERE e.nome_especialidade = 'gastroenterologia'
+  AND i.local_internacao = 'enfermaria'
+  
+SELECT m.nome AS NomeMedico, m.CRM, COUNT(c.id_consulta) AS QuantidadeConsultas
+FROM medico m
+LEFT JOIN consulta c ON m.id_medico = c.id_medico
+GROUP BY m.id_medico
+
+SELECT * FROM medico WHERE nome = "Gabriel";
+
+SELECT e.nome AS NomeEnfermeiro, e.crm AS CRM, COUNT(i.id_internacao) AS NumeroInternacoes
+FROM enfermeiro e
+JOIN participacao p ON e.id_enfermeiro = p.id_enfermeiro
+JOIN internacao i ON p.id_internacao = i.id_internacao
+GROUP BY e.nome, e.crm
+HAVING COUNT(i.id_internacao) > 1
